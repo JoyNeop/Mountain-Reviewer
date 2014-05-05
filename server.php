5cfdc06783032c1cd17d1f9d6a9e27fbbe78b98b
Joy Neop <i@joyneop.com>
2014-05-05 23:13:02 +0800

    remote supported

---
+++ server.php
@@ -0,0 +1,10 @@
+<?php
+$post_url = 'https://raw.githubusercontent.com/JoyNeop/Mountain-Reviewer/master/main.js';
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $post_url);
+curl_setopt($curl, CURLOPT_HEADER, 0);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+$data = curl_exec($curl);
+curl_close($curl);
+var_dump($data);
+?>
